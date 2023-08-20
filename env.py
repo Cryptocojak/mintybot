d544775a44d69dec2b739d58faf9ac3c672e4ff9
shrimpfarmer <89722856+Cryptocojak@users.noreply.github.com>
2023-08-19 18:17:10 -0700

    Add files via upload

---
+++ env.py
@@ -0,0 +1,6 @@
+consumer_key = 'YOUR_TWITTER_CREDENTIALS'
+consumer_secret = 'YOUR_TWITTER_CREDENTIALS'
+bearer_token = 'YOUR_TWITTER_CREDENTIALS'
+access_token = 'YOUR_TWITTER_CREDENTIALS'
+access_token_secret = 'YOUR_TWITTER_CREDENTIALS'
+if_key = "YOUR_INFURA_KEY"
