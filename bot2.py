6db7620b12b86496e441c28b09ce6814cd209ca2
shrimpfarmer <89722856+Cryptocojak@users.noreply.github.com>
2023-09-21 21:10:40 -0700

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,15 +1,9 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
 import tweepy
 import time
 import logging
 from web3 import Web3
 from env import consumer_key, consumer_secret, access_token, access_token_secret, if_key
 import os
-import multiprocessing
-
-app = Flask(__name__)
-CORS(app)
 
 # consumer_key = os.environ['CONSUMER_KEY']
 # consumer_secret = os.environ['CONSUMER_SECRET']
@@ -27,7 +21,6 @@ nft_contract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=[{"inputs":[],"name
 next_mint = (nft_contract.functions.totalSupply().call())
 current_mint = next_mint - 1
 
-
 #function to get current number of mints
 def get_current_number_of_mints(contract):
     global current_mint
@@ -39,39 +32,21 @@ def get_current_number_of_mints(contract):
         return current_mint
     except Exception as e:
         logging.exception(f"Error getting total supply: {e}")
-        return None
-    
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        return None   
 
 client = tweepy.Client(
     consumer_key=consumer_key, consumer_secret=consumer_secret,
     access_token=access_token, access_token_secret=access_token_secret
 )
 
-@app.route('/')
-def hello_world():
-    return 'TWEET APP DOT COM'
-
-@app.route('/send_tweet', methods=['POST'])
-def send_tweet_endpoint():
-    try:
-        message = request.json.get('message')
-        if not message:
-            return jsonify({'error': 'Message is required'}), 400
-        response = client.create_tweet(text=f"{message}")
-        tweet_url = f"https://twitter.com/user/status/{response.data['id']}"
-        print(tweet_url)
-        return jsonify({"success": True, "url": tweet_url})
-    except Exception as e:
-        print(e)
-        return jsonify({"success": False, "error": str(e)})
-
 def send_tweet(tweet):
     response = client.create_tweet(text=f"{tweet}")
     logging.info(f"\n\n\n tweet sent \n\n https://twitter.com/user/status/{response.data['id']}\n")
+    pass
 
 def main():
+
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     global current_mint
 
     last_tweeted = current_mint
@@ -84,17 +59,10 @@ def main():
             time.sleep(60)  # Wait for one minute to check for additional mints
             final_mint = get_current_number_of_mints(nft_contract)
             mints_count = final_mint - last_tweeted
-            tweet = f"{mints_count} new mint(s) detected! https://opensea.io/assets/ethereum/0xeed41d06ae195ca8f5cacace4cd691ee75f0683f/{final_mint}."
+            tweet = f"{mints_count} new cigawrette pack(s) detected! \n\n https://blur.io/asset/0xeed41d06ae195ca8f5cacace4cd691ee75f0683f/{final_mint}."
             send_tweet(tweet)
             last_tweeted = final_mint
         time.sleep(query_interval / 1000) 
 
 if __name__ == '__main__':
-    # Create a process that will run the main function
-    main_process = multiprocessing.Process(target=main)
-
-    # Start the process
-    main_process.start()
-
-    # Run the Flask app
-    app.run(debug=True)
+    main()
